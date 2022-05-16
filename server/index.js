f9f0d049e0131b983325c25b1a4c66107bb6ab64
Rodrigo Saling <rodrigosaling@gmail.com>
2022-05-15 22:43:13 -0300

    added eslint and prettier in monorepo

--- server/index.js
+++ server/index.js
@@ -1,18 +1,21 @@
-import Fastify from 'fastify'
-const fastify = Fastify({
-    logger: true
-})
+import Fastify from 'fastify';
 
+const fastify = Fastify({
+  logger: true,
+});
+
+// eslint-disable-next-line no-unused-vars
 fastify.get('/', async (request, reply) => {
-    return { hello: 'world' }
-})
+  return { hello: 'world' };
+});
 
 const start = async () => {
-    try {
-        await fastify.listen(3000)
-    } catch (err) {
-        fastify.log.error(err)
-        process.exit(1)
-    }
-}
-start()
+  try {
+    await fastify.listen(3001);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
