42abf152c7633b311916910d7f3cd521d68a3cad
Rodrigo Saling <rodrigosaling@gmail.com>
2022-05-28 20:42:40 -0300

    wip testing sqlite and nodemon

--- server/index.js
+++ server/index.js
@@ -1,12 +1,62 @@
 import Fastify from 'fastify';
+import Knex from 'knex';
 
+// const db = new Database('foobar.db');
+
+const knex = Knex({
+  debug: true,
+  client: 'better-sqlite3',
+  connection: {
+    // filename: ':memory:',
+    filename: './dev.sqlite3',
+  },
+});
 const fastify = Fastify({
   logger: true,
 });
 
 // eslint-disable-next-line no-unused-vars
-fastify.get('/', async (request, reply) => {
+fastify.get('/', (request, reply) => {
   return { hello: 'world' };
+});
+
+fastify.get('/create', (request, reply) => {
+  knex.schema
+    .createTable('books', function (table) {
+      table.increments();
+      table.string('title');
+    })
+    .then(function (rows) {
+      console.log('OK', rows);
+    })
+    .catch(function (error) {
+      console.error('ERROR', error);
+    });
+  return 'ok';
+});
+
+fastify.get('/post', (request, reply) => {
+  knex('books')
+    .insert({ title: 'Slaughterhouse Five' })
+    .then(function (rows) {
+      console.log('OK', rows);
+    })
+    .catch(function (error) {
+      console.error('ERROR', error);
+    });
+  return 'ok';
+});
+
+fastify.get('/get', (request, reply) => {
+  return knex.select('*').from('books');
+  // .then(function (rows) {
+  //   console.log('OK', rows);
+  //   return rows;
+  // })
+  // .catch(function (error) {
+  //   console.error('ERROR', error);
+  //   return error;
+  // });
 });
 
 const start = async () => {
