d03578e9ee5d9e90b39ecb75705e7e76a231af7e
Rodrigo Saling <rodrigosaling@gmail.com>
2022-10-09 22:18:37 -0300

    restarting everything once again with react

--- server/src/api/transaction/routes/transaction.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * transaction router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::transaction.transaction');
