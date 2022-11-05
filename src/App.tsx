511d77dff1153e5a910b2fde7bf85cba324ff996
Rodrigo Saling <rodrigosaling@gmail.com>
2022-11-05 19:34:33 -0300

    moving to atilho-web repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
+import React, { useState } from 'react';
+
 function App() {
+  const [accountName, setAccountName] = useState('');
+  const [accounts, setAccounts] = useState(['asdf']);
+
+  const onClick = () => {
+    setAccounts((prevState) => [...prevState, accountName]);
+    setAccountName('');
+  };
+
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1>Hello world!</h1>
+
+      <div>
+        <input
+          type="text"
+          value={accountName}
+          onChange={(e) => setAccountName(e.target.value)}
+        />
+      </div>
+      <button onClick={onClick}>add account</button>
+
+      <h2>Accounts</h2>
+      {accounts.length &&
+        accounts.map((account) => {
+          return <p>{account}</p>;
+        })}
     </div>
   );
 }
